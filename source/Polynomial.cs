c66524faa7c155d46bfa3fdb4afb58ba38e11e2c
electricessence <5899455+electricessence@users.noreply.github.com>
2019-03-03 00:05:05 -0800

    Reformats and improved ref and in usage.

--- source/Polynomial.cs
+++ source/Polynomial.cs
@@ -2,113 +2,113 @@
 
 namespace Open.Numeric.Primes
 {
-	public static class Polynomial
-	{
-		const ulong MAX_ULONG_DIVISOR = 25043747693UL;
+    public static class Polynomial
+    {
+        const ulong MAX_ULONG_DIVISOR = 25043747693UL;
 
-		internal static bool IsPrimeInternal(in ulong value)
-		{
-			ulong divisor = 6;
-			while (divisor * divisor - 2 * divisor + 1 <= value)
-			{
-				if (value % (divisor - 1) == 0)
-					return false;
+        internal static bool IsPrimeInternal(in ulong value)
+        {
+            ulong divisor = 6;
+            while (divisor * divisor - 2 * divisor + 1 <= value)
+            {
+                if (value % (divisor - 1) == 0)
+                    return false;
 
-				if (value % (divisor + 1) == 0)
-					return false;
+                if (value % (divisor + 1) == 0)
+                    return false;
 
-				divisor += 6;
+                divisor += 6;
 
-				if (divisor > MAX_ULONG_DIVISOR)
-					return IsPrime(value, divisor);
-			}
-			return true;
-		}
+                if (divisor > MAX_ULONG_DIVISOR)
+                    return IsPrime(value, divisor);
+            }
+            return true;
+        }
 
-		/// <summary>
-		/// Returns true if the value provided is prime.
-		/// </summary>
-		/// <param name="value">The value to validate.</param>
-		/// <returns>True if the value provided is prime</returns>
-		public static bool IsPrime(in ulong value)
-		{
-			switch (value)
-			{
-				// 0 and 1 are not prime numbers
-				case 0UL:
-				case 1UL:
-					return false;
-				case 2UL:
-				case 3UL:
-					return true;
+        /// <summary>
+        /// Returns true if the value provided is prime.
+        /// </summary>
+        /// <param name="value">The value to validate.</param>
+        /// <returns>True if the value provided is prime</returns>
+        public static bool IsPrime(in ulong value)
+        {
+            switch (value)
+            {
+                // 0 and 1 are not prime numbers
+                case 0UL:
+                case 1UL:
+                    return false;
+                case 2UL:
+                case 3UL:
+                    return true;
 
-				default:
+                default:
 
-					if (value % 2 == 0 || value % 3 == 0)
-						return false;
+                    if (value % 2 == 0 || value % 3 == 0)
+                        return false;
 
-					return IsPrimeInternal(in value);
-			}
-		}
+                    return IsPrimeInternal(in value);
+            }
+        }
 
-		/// <summary>
-		/// Returns true if the value provided is prime.
-		/// </summary>
-		/// <param name="value">The value to validate.</param>
-		/// <returns>True if the value provided is prime</returns>
-		public static bool IsBigPrime(in BigInteger value)
-		{
-			if (value.IsZero)
-				return false;
+        /// <summary>
+        /// Returns true if the value provided is prime.
+        /// </summary>
+        /// <param name="value">The value to validate.</param>
+        /// <returns>True if the value provided is prime</returns>
+        public static bool IsBigPrime(in BigInteger value)
+        {
+            if (value.IsZero)
+                return false;
 
-			bool primeCheck(in BigInteger v)
-			{
-				if (v == BIG.TWO || v == BIG.THREE)
-					return true;
+            bool primeCheck(in BigInteger v)
+            {
+                if (v == BIG.TWO || v == BIG.THREE)
+                    return true;
 
-				if (v.IsEven)
-					return false;
+                if (v.IsEven)
+                    return false;
 
-				if (v <= ulong.MaxValue)
-					return IsPrime((ulong)v);
+                if (v <= ulong.MaxValue)
+                    return IsPrime((ulong)v);
 
-				return v % 3 != 0
-					   && IsPrime(v, 6);
-			}
+                return v % 3 != 0
+                       && IsPrime(v, 6);
+            }
 
-			return value.Sign == -1
-				? value != BigInteger.MinusOne && primeCheck(BigInteger.Abs(value))
-				: !value.IsOne && primeCheck(in value);
-		}
+            return value.Sign == -1
+                ? value != BigInteger.MinusOne && primeCheck(BigInteger.Abs(value))
+                : !value.IsOne && primeCheck(in value);
+        }
 
-		internal static bool IsPrime(in BigInteger value, BigInteger divisor)
-		{
-			while (divisor * divisor - 2 * divisor + 1 <= value)
-			{
-				if (value % (divisor - 1) == 0)
-					return false;
+        internal static bool IsPrime(in BigInteger value, BigInteger divisor)
+        {
+            while (divisor * divisor - 2 * divisor + 1 <= value)
+            {
+                if (value % (divisor - 1) == 0)
+                    return false;
 
-				if (value % (divisor + 1) == 0)
-					return false;
+                if (value % (divisor + 1) == 0)
+                    return false;
 
-				divisor += 6;
-			}
+                divisor += 6;
+            }
 
-			return true;
-		}
+            return true;
+        }
 
-		public class U64 : PrimalityU64Base
-		{
-			// ReSharper disable once MemberHidesStaticFromOuterClass
-			protected override bool IsPrimeInternal(in ulong value)
-			=> Polynomial.IsPrimeInternal(in value);
-		}
+        public class U64 : PrimalityU64Base
+        {
+            // ReSharper disable once MemberHidesStaticFromOuterClass
+            protected override bool IsPrimeInternal(in ulong value)
+            => Polynomial.IsPrimeInternal(in value);
+        }
 
-		public class BigInt : PrimalityBigIntBase
-		{
-			protected override bool IsPrimeInternal(in BigInteger value)
-			=> Polynomial.IsPrime(in value, 6UL);
-		}
-	}
+        public class BigInt : PrimalityBigIntBase
+        {
+            protected override bool IsPrimeInternal(in BigInteger value)
+            => Polynomial.IsPrime(in value, 6UL);
+        }
+    }
 
 }
--- source/TrialDivision.cs
+++ source/TrialDivision.cs
@@ -6,128 +6,128 @@ using System.Threading;
 
 namespace Open.Numeric.Primes
 {
-	public class TrialDivision : PrimalityU64Base
-	{
-		public static readonly IReadOnlyList<ulong> FirstKnown
-			= (new List<ulong>() { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, })
-				.AsReadOnly();
+    public class TrialDivision : PrimalityU64Base
+    {
+        public static readonly IReadOnlyList<ulong> FirstKnown
+            = (new List<ulong>() { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, })
+                .AsReadOnly();
 
-		/// <inheritdoc />
-		public override ParallelQuery<ulong> InParallel(in ulong staringAt, ushort? degreeOfParallelism = null)
-		{
-			var sa = staringAt;
-			var tests = AllNumbers().SkipWhile(v => v < sa) // This is the difference.
-				.AsParallel().AsOrdered();
+        /// <inheritdoc />
+        public override ParallelQuery<ulong> InParallel(in ulong staringAt, ushort? degreeOfParallelism = null)
+        {
+            var sa = staringAt;
+            var tests = AllNumbers().SkipWhile(v => v < sa) // This is the difference.
+                .AsParallel().AsOrdered();
 
-			if (degreeOfParallelism.HasValue)
-				tests = tests.WithDegreeOfParallelism(degreeOfParallelism.Value);
+            if (degreeOfParallelism.HasValue)
+                tests = tests.WithDegreeOfParallelism(degreeOfParallelism.Value);
 
-			return tests.Where(v => IsPrime(in v));
-		}
+            return tests.Where(v => IsPrime(in v));
+        }
 
-		// ReSharper disable once UnusedMember.Local
-		const ulong MAX_ULONG_SQUARE_ROOT = 4294967296;
+        // ReSharper disable once UnusedMember.Local
+        const ulong MAX_ULONG_SQUARE_ROOT = 4294967296;
 
-		protected override bool IsPrimeInternal(in ulong value)
-		{
-			var sqr = (ulong)Math.Sqrt(value);
+        protected override bool IsPrimeInternal(in ulong value)
+        {
+            var sqr = (ulong)Math.Sqrt(value);
 
-			foreach (var p in AllNumbers())
-			{
-				if (p > sqr || value == p)
-					return true;
-				if (value % p == 0)
-					return false;
-			}
+            foreach (var p in AllNumbers())
+            {
+                if (p > sqr || value == p)
+                    return true;
+                if (value % p == 0)
+                    return false;
+            }
 
-			throw new Exception("Unexpectedly exited prime test loop.");
-		}
+            throw new Exception("Unexpectedly exited prime test loop.");
+        }
 
-		protected virtual IEnumerable<ulong> AllNumbers()
-		{
-			var known = new LinkedList<ulong>();
-			var last = 0UL;
-			foreach (var k in FirstKnown)
-			{
-				yield return k;
-				last = k;
-				known.AddLast(k);
-			}
+        protected virtual IEnumerable<ulong> AllNumbers()
+        {
+            var known = new LinkedList<ulong>();
+            var last = 0UL;
+            foreach (var k in FirstKnown)
+            {
+                yield return k;
+                last = k;
+                known.AddLast(k);
+            }
 
-			foreach (var n in ValidPrimeTests(last + 2))
-			{
-				last = 1L;
-				var pN = known.First;
-				do
-				{
-					var p = pN.Value;
-					var stop = n / last; // The list of possibilities shrinks for each test.
-					if (p > stop)
-					{
-						known.AddLast(n);
-						yield return n;
-						pN = null;
-					}
-					else if (n == p || (n % p) == 0)
-					{
-						pN = null;
-					}
-					else
-					{
-						pN = pN.Next;
-					}
-					last = p;
-				}
-				while (pN != null);
-			}
-		}
+            foreach (var n in ValidPrimeTests(last + 2))
+            {
+                last = 1L;
+                var pN = known.First;
+                do
+                {
+                    var p = pN.Value;
+                    var stop = n / last; // The list of possibilities shrinks for each test.
+                    if (p > stop)
+                    {
+                        known.AddLast(n);
+                        yield return n;
+                        pN = null;
+                    }
+                    else if (n == p || (n % p) == 0)
+                    {
+                        pN = null;
+                    }
+                    else
+                    {
+                        pN = pN.Next;
+                    }
+                    last = p;
+                }
+                while (pN != null);
+            }
+        }
 
-		public override IEnumerator<ulong> GetEnumerator()
-			=> AllNumbers().GetEnumerator();
+        public override IEnumerator<ulong> GetEnumerator()
+            => AllNumbers().GetEnumerator();
 
-		/// <inheritdoc />
-		/// <summary>
-		/// Returns an enumerable that will iterate every prime starting at the starting value.
-		/// </summary>
-		/// <param name="startingAt">Allows for skipping ahead any integer before checking for inclusive and subsequent primes.</param>
-		/// <returns>An enumerable that will iterate every prime starting at the starting value</returns>
-		public override IEnumerable<ulong> StartingAt(ulong startingAt)
-			=> AllNumbers().SkipWhile(n => n < startingAt);
+        /// <inheritdoc />
+        /// <summary>
+        /// Returns an enumerable that will iterate every prime starting at the starting value.
+        /// </summary>
+        /// <param name="startingAt">Allows for skipping ahead any integer before checking for inclusive and subsequent primes.</param>
+        /// <returns>An enumerable that will iterate every prime starting at the starting value</returns>
+        public override IEnumerable<ulong> StartingAt(ulong startingAt)
+            => AllNumbers().SkipWhile(n => n < startingAt);
 
-		protected override bool IsFactorable(in ulong value)
-			=> true; // Do not do prime check first.
-	}
+        protected override bool IsFactorable(in ulong value)
+            => true; // Do not do prime check first.
+    }
 
-	public class TrialDivisionMemoized : TrialDivision
-	{
+    public class TrialDivisionMemoized : TrialDivision
+    {
 
-		LazyList<ulong> Memoized;
+        LazyList<ulong> Memoized;
 
-		/// <summary>
-		/// Returns a memoized enumerable that will iterate every prime starting at the starting value.
-		/// </summary>
-		/// <returns>A memoized enumerable that will iterate every prime starting at the starting value</returns>
-		protected override IEnumerable<ulong> AllNumbers()
-			=> LazyInitializer
-				.EnsureInitialized(ref Memoized,
-					() => NumbersMemoizable().Memoize());
+        /// <summary>
+        /// Returns a memoized enumerable that will iterate every prime starting at the starting value.
+        /// </summary>
+        /// <returns>A memoized enumerable that will iterate every prime starting at the starting value</returns>
+        protected override IEnumerable<ulong> AllNumbers()
+            => LazyInitializer
+                .EnsureInitialized(ref Memoized,
+                    () => NumbersMemoizable().Memoize());
 
-		protected IEnumerable<ulong> NumbersMemoizable()
-		{
-			ulong last = 1;
-			foreach (var n in FirstKnown)
-			{
-				yield return n;
-				last = n;
-			}
+        protected IEnumerable<ulong> NumbersMemoizable()
+        {
+            ulong last = 1;
+            foreach (var n in FirstKnown)
+            {
+                yield return n;
+                last = n;
+            }
 
-			/*
+            /*
              * Note: here is where things start to recurse but should work perfectly
              * as the next primes can only be discovered by their predecessors.
              */
-			foreach (var n in ValidPrimeTests(last + 1).Where(v => IsPrime(in v)))
-				yield return n;
-		}
+            foreach (var n in ValidPrimeTests(last + 1).Where(v => IsPrime(in v)))
+                yield return n;
+        }
 
-	}
+    }
 }
