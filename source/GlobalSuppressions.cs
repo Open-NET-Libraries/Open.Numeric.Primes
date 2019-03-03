faf2540fda29fe2d989f8ee27a658149fb9f3ac5
electricessence <5899455+electricessence@users.noreply.github.com>
2019-03-03 00:05:29 -0800

    Update GlobalSuppressions.cs

--- source/GlobalSuppressions.cs
+++ source/GlobalSuppressions.cs
@@ -1,6 +1,7 @@
 ﻿
 // This file is used by Code Analysis to maintain SuppressMessage 
 // attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:Open.Numeric.Primes.MillerRabin.Mul(System.Numerics.BigInteger@,System.Numerics.BigInteger@,System.Numerics.BigInteger@)~System.Numerics.BigInteger")]
